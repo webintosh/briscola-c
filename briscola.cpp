278a64368644b6e1edceb133fc38038b88637224
webintosh <106496390+webintosh@users.noreply.github.com>
2022-09-21 11:32:41 +0200

    Update briscola.cpp

--- briscola.cpp
+++ briscola.cpp
@@ -4,23 +4,39 @@
 #include"math.h"
 int mazzo[40];
 
+int mazzog1[40];
+int mazzog2[40];
+
+void inizializzagiocatori{
+	//svuoto i mazzi di guadagno dei giocatori
+	for (int i = 0; i < 40; i++) {    
+    mazzog1[i] = 0;
+
+	}
+	for (int i = 0; i < 40; i++) {    
+    mazzog2[i] = 0;
+
+	}
+
+}
+
 
 void faiilmazzo(){
-	
+	//riempio e poi randomizzo le posizioni del mazzo
 
-	for (int i = 0; i < 40; i++) {     // fill array
+	for (int i = 0; i < 40; i++) {    
     mazzo[i] = i+1;
     //printf("%d,", mazzo[i]);
 	}
 
 
-	for (int i = 0; i < 40; i++) {    // shuffle array
+	for (int i = 0; i < 40; i++) {   
     	int temp = mazzo[i];
     	int randomIndex = rand() % 40;
 
     	mazzo[i]           = mazzo[randomIndex];
     	mazzo[randomIndex] = temp;
-}
+	}
 		
 	
 }
